7be515d42522b5d16b3a294f092660d550a23c87
Christina Takara <christina.takara@gmail.com>
2023-11-26 13:10:40 -0600

    display attributes

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
 function ListItem(props) {
-  return (
-    <div>
-        ListItem
-        <h5>{props.item.data[0].title}</h5>
-    </div>
-  )
+    return (
+        <div>
+            <img src={props.item.links?.[0].href} alt={props.item.data[0].title} />
+
+            < div>
+                <h5>{props.item.data[0].title}</h5>
+                <p>Location: {props.item.data[0].location}</p>
+                <p>Created: {props.item.data[0].date_created}</p>
+            </div>
+
+        </div>
+    )
 }
 
 export default ListItem
